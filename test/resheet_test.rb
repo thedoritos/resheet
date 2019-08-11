fedcf5107f7807b9a376b21c7685a42b94772143
thedoritos <thedoritos@gmail.com>
2019-08-12 04:51:44 +0900

    :white_check_mark: Test create record

--- test/resheet_test.rb
+++ test/resheet_test.rb
@@ -1,5 +1,6 @@
 require "test/unit"
 require "rack/test"
+require "json"
 
 OUTER_APP = Rack::Builder.parse_file('config.ru').first
 
@@ -8,6 +9,18 @@ class HomepageTest < Test::Unit::TestCase
 
   def app
     OUTER_APP
+  end
+
+  def header
+    { 'CONTENT_TYPE' => 'application/json' }
+  end
+
+  def json(params)
+    JSON.generate(params)
+  end
+
+  def teardown
+    delete '/animations/2'
   end
 
   def test_select_resource
@@ -22,5 +35,13 @@ class HomepageTest < Test::Unit::TestCase
 
     assert last_response.ok?
     assert_equal last_response.body, '{"id":"1","title":"SHIROBAKO"}'
+  end
+
+  def test_create_resource
+    params = { title: "Charlotte" }
+    post '/animations', json(params), header
+
+    assert last_response.created?
+    assert_equal last_response.header['Location'], '/animations/2'
   end
 end
