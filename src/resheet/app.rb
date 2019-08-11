ce78766280d9dbc2bc3ec97e36c4be3e391cf121
thedoritos <thedoritos@gmail.com>
2019-08-12 06:16:42 +0900

    :art: Add create process

--- src/resheet/app.rb
+++ src/resheet/app.rb
@@ -2,6 +2,7 @@ require 'googleauth'
 require 'google/apis/sheets_v4'
 require 'json'
 require 'resheet/request'
+require 'resheet/process/create'
 require 'resheet/process/select'
 require 'resheet/process/find'
 require 'resheet/process/update'
@@ -23,32 +24,10 @@ class Resheet::App
 
     service = Google::Apis::SheetsV4::SheetsService.new
     service.authorization = credentials
-    begin
-      values = service.get_spreadsheet_values(SHEET_ID, "#{request.resource}!A:Z").values
-    rescue Google::Apis::ClientError => error
-      return [500, { 'Content-Type' => 'application/json' }, ["{ \"error\": \"#{error.class}: #{error}\" }"]]
-    end
 
-    header = values[0]
-    rows = values.drop(1)
-    data = rows.map do |row|
-      header.each_with_index.map { |key, i| [key, row[i]] }.to_h
-    end
-
-    if request.method == 'POST'
-      new_record = header.map { |key| [key, request.params[key]] }.to_h
-      new_record['id'] = data.map { |item| item['id'].to_i }.max + 1
-
-      update = Google::Apis::SheetsV4::ValueRange.new
-      update.range = "#{request.resource}!A:Z"
-      update.values = [new_record.values]
-
-      service.append_spreadsheet_value(SHEET_ID, update.range, update, value_input_option: "USER_ENTERED")
-
-      return [201, { 'Content-Type' => 'application/json', 'Location' => "/#{request.resource}/#{new_record['id']}" }, []]
-    end
-
-    process = if ['PUT', 'PATCH'].include?(request.method)
+    process = if request.method == 'POST'
+      Resheet::Process::Create.new(service, SHEET_ID)
+    elsif ['PUT', 'PATCH'].include?(request.method)
       Resheet::Process::Update.new(service, SHEET_ID)
     elsif request.method == 'DELETE'
       Resheet::Process::Delete.new(service, SHEET_ID)
---
+++ src/resheet/process/create.rb
@@ -0,0 +1,33 @@
+module Resheet::Process
+  class Create
+    def initialize(sheet_service, spreadsheet_id)
+      @service = sheet_service
+      @spreadsheet_id = spreadsheet_id
+    end
+
+    def receive(request)
+      begin
+        values = @service.get_spreadsheet_values(@spreadsheet_id, "#{request.resource}!A:Z").values
+      rescue Google::Apis::ClientError => error
+        return [500, { 'Content-Type' => 'application/json' }, ["{ \"error\": \"#{error.class}: #{error}\" }"]]
+      end
+
+      header = values[0]
+      rows = values.drop(1)
+      data = rows.map do |row|
+        header.each_with_index.map { |key, i| [key, row[i]] }.to_h
+      end
+
+      new_record = header.map { |key| [key, request.params[key]] }.to_h
+      new_record['id'] = data.map { |item| item['id'].to_i }.max + 1
+
+      update = Google::Apis::SheetsV4::ValueRange.new
+      update.range = "#{request.resource}!A:Z"
+      update.values = [new_record.values]
+
+      @service.append_spreadsheet_value(@spreadsheet_id, update.range, update, value_input_option: "USER_ENTERED")
+
+      return [201, { 'Content-Type' => 'application/json', 'Location' => "/#{request.resource}/#{new_record['id']}" }, []]
+    end
+  end
+end
