89448437a813362577c1151f39ec1037d6b9affb
thedoritos <thedoritos@gmail.com>
2019-08-12 16:15:26 +0900

    :art: Fetch data by sheet class

--- src/resheet/action/create.rb
+++ src/resheet/action/create.rb
@@ -1,31 +1,27 @@
+require 'resheet/sheet'
+
 module Resheet::Action
   class Create
-    def initialize(sheet_service, spreadsheet_id)
-      @service = sheet_service
+    def initialize(sheets_service, spreadsheet_id)
+      @sheets_service = sheets_service
       @spreadsheet_id = spreadsheet_id
     end
 
     def invoke(request)
-      begin
-        values = @service.get_spreadsheet_values(@spreadsheet_id, "#{request.resource}!A:Z").values
-      rescue Google::Apis::ClientError => error
-        return [500, { 'Content-Type' => 'application/json' }, ["{ \"error\": \"#{error.class}: #{error}\" }"]]
+      sheet = Resheet::Sheet.new(@sheets_service, @spreadsheet_id, request.resource)
+      sheet.fetch
+
+      if sheet.error
+        return [500, { 'Content-Type' => 'application/json' }, ["{ \"error\": \"#{sheet.error.class}: #{sheet.error}\" }"]]
       end
 
-      header = values[0]
-      rows = values.drop(1)
-      data = rows.map do |row|
-        header.each_with_index.map { |key, i| [key, row[i]] }.to_h
-      end
-
-      new_record = header.map { |key| [key, request.params[key]] }.to_h
-      new_record['id'] = data.map { |item| item['id'].to_i }.max + 1
+      new_record = sheet.new_record(request.params)
 
       update = Google::Apis::SheetsV4::ValueRange.new
       update.range = "#{request.resource}!A:Z"
       update.values = [new_record.values]
 
-      @service.append_spreadsheet_value(@spreadsheet_id, update.range, update, value_input_option: "USER_ENTERED")
+      @sheets_service.append_spreadsheet_value(@spreadsheet_id, update.range, update, value_input_option: "USER_ENTERED")
 
       return [201, { 'Content-Type' => 'application/json', 'Location' => "/#{request.resource}/#{new_record['id']}" }, []]
     end
---
+++ src/resheet/sheet.rb
@@ -0,0 +1,31 @@
+module Resheet
+  class Sheet
+    attr_reader :error
+    attr_reader :header, :rows, :data
+
+    def initialize(sheets_service, spreadsheet_id, resource)
+      @sheets_service = sheets_service
+      @spreadsheet_id = spreadsheet_id
+      @resource = resource
+    end
+
+    def fetch
+      begin
+        values = @sheets_service.get_spreadsheet_values(@spreadsheet_id, "#{@resource}!A:Z").values
+        @header = values[0]
+        @rows = values.drop(1)
+        @data = @rows.map do |row|
+          @header.each_with_index.map { |key, i| [key, row[i]] }.to_h
+        end
+      rescue Google::Apis::ClientError => error
+        @error = error
+      end
+    end
+
+    def new_record(params)
+      record = @header.map { |key| [key, params[key]] }.to_h
+      record['id'] = @data.map { |item| item['id'].to_i }.max + 1
+      record
+    end
+  end
+end
